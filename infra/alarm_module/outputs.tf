febb1cb5da218a4c81ccd6d8ca2b4d76a5c3f213
ArianMathai <123459777+ArianMathai@users.noreply.github.com>
2024-11-05 15:29:02 +0100

    New gauge and cloudwatch metric for num of accounts over 2m v4

--- infra/alarm_module/outputs.tf
+++ infra/alarm_module/outputs.tf
@@ -3,5 +3,5 @@ output "alarm_arn" {
 }
 
 output "account_over_2m_alarm_arn" {
-  value = aws_cloudwatch_metric_alarm.account_over_2m_alarm.arn
+  value = aws_cloudwatch_metric_alarm.account_count_over_2m.arn
 }
